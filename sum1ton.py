e42065bc4aeb8ef8e66fe8695531a7c90ead6e85
faleirol <faleirol@stu.otc.school.nz>
2021-03-25 08:57:40 +1300

    here

--- sum1ton.py
+++ sum1ton.py
@@ -1,2 +1,8 @@
 n = int(input("Enter a number"))
+total = 0
 # /n is in there at the end of a print statement
+for i in range(n):
+    print(i + 1)
+    i = i +1
+    total = total + i
+print("The total is :" , total)
